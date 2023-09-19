81a73cf6e4cb4610b3c369778e77d9118038e88e
JingHuangSu1996 <jing.tech.tw@gmail.com>
2023-09-20 01:27:23 +0800

    chore: update lint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+const ERROR = 'error';
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  plugins: [],
+  rules: {
+    'default-case': ERROR,
+    'default-case-last': ERROR,
+    'dot-notation': [
+      ERROR,
+      {
+        allowKeywords: false,
+      },
+    ],
+    'no-confusing-arrow': ERROR,
+    'no-duplicate-imports': ERROR,
+    'no-empty-function': ERROR,
+    'no-negated-condition': ERROR,
+    'no-self-compare': ERROR,
+    'no-template-curly-in-string': ERROR,
+  },
+  env: {
+    jest: true,
+    node: true,
+  },
+  ignorePatterns: ['node_modules'],
+};
