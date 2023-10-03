60a850fa128cdeebd6da4e98e9fb779f2134e5ba
JingHuangSu1996 <jing.tech.tw@gmail.com>
2023-10-03 23:42:06 +0800

    feat(ripple): init

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,17 @@
 import type { Preview } from '@storybook/react';
 
+export const globalTypes = {
+  'storybook-addon-theme-switcher/theme': {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['dark', 'light'],
+    },
+  },
+};
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -8,6 +20,10 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    themeSwitcher: {
+      themes: ['light', 'dark'],
+      dataAttribute: 'theme',
     },
   },
 };
---
+++ packages/components/ripple/src/Ripple.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+
+test('renders Ripple component', () => {
+  render(<span></span>);
+  // Add your test assertions here
+});
