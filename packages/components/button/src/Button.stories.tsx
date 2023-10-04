147b5168e10784825c5b070220d8af8762386ac6
JingHuangSu1996 <jing.tech.tw@gmail.com>
2023-10-04 23:38:50 +0800

    feat(button): css

--- packages/components/button/src/Button.stories.tsx
+++ packages/components/button/src/Button.stories.tsx
@@ -1,21 +1,30 @@
 import * as React from 'react';
 import { Button as Component, ButtonProps } from './';
 
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 
 const storyMetadata: Meta<typeof Component> = {
   title: 'Button',
   component: Component,
+  argTypes: {},
 };
 
 export default storyMetadata;
 
-const Template: StoryFn<typeof Component> = (args: ButtonProps) => {
-  return <Component {...args}>Elevated Button</Component>;
+const variants = ['elevated', 'outline', 'text', 'filled', 'filled-tonal'] as const;
+
+const Template = (args: ButtonProps) => {
+  return variants.map((variant) => {
+    return (
+      <div style={{ display: 'inline-flex', marginRight: 10 }}>
+        <Component {...args} variant={variant}>
+          Label
+        </Component>
+      </div>
+    );
+  });
 };
 
-export const Button: StoryFn<typeof Component> = Template.bind({});
+export const Button = Template.bind({});
 
-Button.args = {
-  variant: 'elevated',
-};
+Button.args = {};
--- packages/components/button/src/Button.tsx
+++ packages/components/button/src/Button.tsx
@@ -4,7 +4,7 @@ import '@tocino-ui/css/dist/button.css';
 
 export type ButtonProps = {
   /** Add a description comment for each prop. */
-  variant: 'elevated' | 'outlined' | 'text';
+  variant: 'elevated' | 'outline' | 'text' | 'filled' | 'filled-tonal';
 };
 
 export const Button = React.forwardRef<HTMLButtonElement, PropsWithChildren<ButtonProps>>((props) => {
@@ -13,8 +13,8 @@ export const Button = React.forwardRef<HTMLButtonElement, PropsWithChildren<Butt
 
   return (
     <button className="tocino-Button" data-variant={variant} ref={btnRef}>
-      <span className="button-label">{children}</span>
-      <Ripple target={btnRef} color="rgba(0, 0, 0, 0.1)" />
+      <span className="tocino-Button__label">{children}</span>
+      <Ripple target={btnRef} />
     </button>
   );
 });
